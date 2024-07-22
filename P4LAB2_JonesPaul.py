ca1102fccc9914f2b53fd72c0b5abf3f4557bd3f
TyXanta <jonesp9764@student.faytechcc.edu>
2024-07-22 05:15:46 -0400

    Add files via upload

---
+++ P4LAB2_JonesPaul.py
@@ -0,0 +1,15 @@
+# Paul Jones
+# 7/22/2024
+# P4LAB2
+#Loops
+
+x = int(input('Enter an integer: '))
+i = range
+print()
+
+for i in range(1,13):
+    print(f' {x} * {i} = {x*i}')
+print()
+n =(input('Would you like to run the program again?'))
+if n = no:
+    print('Exiting program ...')
