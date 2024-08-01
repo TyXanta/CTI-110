df626615f8f9ba5bfa48bb485dfec8127c41ed14
TyXanta <jonesp9764@student.faytechcc.edu>
2024-08-01 19:54:10 -0400

    Add files via upload

---
+++ P1HW1_JonesPaul.py
@@ -0,0 +1,36 @@
+# Paul Jones
+# 8/1/2024
+# P1HW1
+# Calculations
+
+# 1st half
+
+print("-----Calculating Exponents----")
+print()
+print()
+
+base = int(input("Enter an integer as the base value: "))
+expo = int(input("Enter an integer as the exponent: "))
+solution = (base)**(expo)
+
+print()
+print()
+print(f'{base} raised to the power of {expo} is {solution} !!')
+print()
+print()
+
+print("-----Addition and Subtraction----")
+print()
+print()
+
+# 2nd half
+
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subt = int(input("Enter an integer to subtract: "))
+total = (start + add) - (subt)
+
+print()
+print()
+
+print(f'{start} + {add} - {subt} is equal to {total}')
