0b2cfe491f0abceba1ec5ce0255ac72115305ae9
TyXanta <jonesp9764@student.faytechcc.edu>
2024-08-01 18:36:50 -0400

    Add files via upload

---
+++ P1LAB1_JonesPaul.py
@@ -0,0 +1,13 @@
+# Paul Jones
+# 8/1/2024
+# P1LAB1
+# Introduction Program
+
+# Terms
+
+firstn = (input("Enter your first name: "))
+lastn = (input("Enter your last name: "))
+
+# Product
+
+print(f"Hello, {firstn} {lastn}! Welcome to CTI-110")
